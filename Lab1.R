5203f95ce788d512f735135d1651b2b846540809
Prettmass <71295896+Prettmass@users.noreply.github.com>
2020-09-29 22:04:05 -0400

    Add files via upload

---
+++ Lab1.R
@@ -0,0 +1,14 @@
+
+#Lab 1
+
+sum(sample(x = 1:6, size = 100, replace = TRUE))
+
+x = (1+2+3+4+5+6)/6
+
+sd((sample(x = 1:6, size = 100, replace = TRUE)))
+
+#The sample mean and sample Standard dev used in the minimum sample formula
+
+
+
+
